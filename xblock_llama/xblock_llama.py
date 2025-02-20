d38d3ddb8eee000189b2b3bfbc2aac913b9be6a6
DONG QIAN <qdong@cn.ibm.com>
2025-02-21 04:02:09 +0800

    change static/html to templates/*.html

--- xblock_llama/xblock_llama.py
+++ xblock_llama/xblock_llama.py
@@ -35,10 +35,10 @@ class LlamaXBlock(XBlock):
         context = context or {}  # 初始化 context
         context['prompt'] = self.prompt  # 从 XBlock 字段中获取 prompt  
         context['response'] = self.response  # 将 response 字段添加到 context 中 
-        html = self.render_template("/static/html/student_view.html", context) 
+        html = self.render_template("templates/student_view.html", context) 
         frag = Fragment(html)
-        frag.add_css(self.resource_string("/static/css/xblock_llama.css"))
-        frag.add_javascript(self.resource_string("/static/js/src/xblock_llama.js"))
+        frag.add_css(self.resource_string("static/css/xblock_llama.css"))
+        frag.add_javascript(self.resource_string("static/js/xblock_llama.js"))
         frag.initialize_js('LlamaXBlock', json_args=self.get_context())    
         return frag
 
@@ -48,10 +48,10 @@ class LlamaXBlock(XBlock):
         context['model_type'] = self.model_type
         context['deepseek_api_key'] = self.deepseek_api_key
 
-        html = self.render_template("studio_view.html", context)
+        html = self.render_template("templates/studio_view.html", context)
         frag = Fragment(html)
-        frag.add_css(self.resource_string("/static/css/xblock_llama.css"))
-        frag.add_javascript(self.resource_string("/static/js/src/xblock_llama.js"))
+        frag.add_css(self.resource_string("static/css/xblock_llama.css"))
+        frag.add_javascript(self.resource_string("static/js/xblock_llama.js"))
         frag.initialize_js('LlamaXBlock', json_args=self.get_context())  
 
         return frag
